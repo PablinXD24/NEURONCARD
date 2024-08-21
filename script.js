e0f015ce0371bd961cd215a61425c2675cbc8d30
PablinXD24 <pablo11ssousa2@gmail.com>
2024-08-21 11:51:25 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const card = document.getElementById('card');
+    const url = 'https://pablinxd24.github.io/NEURON3/'; // Substitua com o URL desejado
+
+    card.addEventListener('click', () => {
+        window.location.href = url;
+    });
+});
